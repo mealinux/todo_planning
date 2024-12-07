4134d9f1fa7faf237670ad0b8b8a2f7f729c6779
mealinux <alinux1994@gmail.com>
2024-12-07 20:34:05 +0300

    feat: created a command that call todo list

---
+++ app/Console/Commands/FetchTodoList.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FetchTodoList extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch the Work To-Do List';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
